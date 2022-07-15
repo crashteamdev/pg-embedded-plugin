998f046e099042a1b134a8ca9801e79949da1fa4
vitaxa <vitaxa93gamebox@gmail.com>
2022-07-15 13:11:53 +0300

    Initial commit (#1)

---
+++ src/main/java/dev/crashteam/maven/plugins/pg/embedded/plugin/StopPgServerMojo.java
@@ -0,0 +1,34 @@
+package dev.crashteam.maven.plugins.pg.embedded.plugin;
+
+import io.zonky.test.db.postgres.embedded.EmbeddedPostgres;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+import java.io.IOException;
+
+/**
+ * Class to stop the embedded server
+ */
+@Mojo(name = "stop", defaultPhase = LifecyclePhase.COMPILE, threadSafe = true)
+public class StopPgServerMojo extends GeneralMojo {
+
+    /** Instance of the PostgreSQL */
+    private EmbeddedPostgres embeddedPostgres;
+
+    @Override
+    protected void doExecute() throws MojoExecutionException, MojoFailureException {
+        try {
+            if (StartPgServerMojo.isRunning()) {
+                getLog().info("Stopping the PostgreSQL server...");
+                StartPgServerMojo.stopPgServer();
+                getLog().info("The PostgreSQL server stopped");
+            } else {
+                getLog().info("The PostgreSQL server wasn't started!");
+            }
+        } catch (IOException e) {
+            getLog().error("Error encountered while stopping the server ", e);
+        }
+    }
+}
